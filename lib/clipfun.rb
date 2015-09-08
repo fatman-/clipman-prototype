b4b190ee33b5f52d86021a75265242185980ad42
Sai Kishore K <saikishore.bits@gmail.com>
2015-09-09 00:49:57 +0530

    Document code

--- lib/clipfun.rb
+++ lib/clipfun.rb
@@ -2,16 +2,21 @@
 
 module Clipboard
 
-  # Provides finctions for readong and writing to the system clipboard
-  # Currently only implemented for the Mac OS
+  # Provides functions for reading and writing to the system clipboard
+  # Currently, only implemented for the Mac OS
 
   def self.copy(input)
+    # Using Mac OS's command line tool 'pbcopy' for copying input text
+    # input - text string to be copied to the system clipboard
+
     str = input.to_s
     IO.popen('pbcopy', 'w') { |f| f << str }
     str
   end
 
   def self.paste
+    # Using Mac OS's command line tool 'pbpaste' for pasting the current clip
+
     `pbpaste`
   end
 
--- test.rb
+++ test.rb
@@ -1,3 +1,6 @@
+# code for testing out whether the data is properly being recorded,
+# by printing out the data recorded
+
 clips_file = File.join(__dir__, 'data/cliprec.bin')
 unless File.exist? clips_file
   cr_clips = Array.new
